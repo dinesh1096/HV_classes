b7a14c79042e596085c89c7499a4b7d344837154
d.dinesh <dinu1096@gmail.com>
2022-12-06 11:34:59 +0530

    First commit

---
+++ first.py
@@ -0,0 +1,48 @@
+# noofCars=0
+# noofBikes=0
+# totalPayment=0
+# noofCars=int(input("Enter how many cars parked"))
+# noofBikes=int(input("Enter how many bikes parked"))
+# totalPayment= noofBikes*20 + noofCars*40;
+# print("total amount would be",totalPayment)
+
+
+
+
+# CAMEL casing
+# noOfCars = 0
+# noOfBikes = 0
+# totalPayment = 0
+# noOfCars = int(input("No of Cars"))
+# noOfBikes = int(input("No of Bikes"))
+# totalPayment = noOfBikes*20+noOfCars*40
+# print("total payment would be", totalPayment)
+# noOfCars="Sanjoy"
+#type independent
+#Store management Software
+
+#3 variables : Product 1,Product 2, product 3
+#product1 =40
+#product2=50
+#product3=60
+
+product1=40
+product2=50
+product3=60
+product1=int(input("Enter how many kgs you want"))
+product2=int(input("Enter how many kgs you want"))
+product3=int(input("Enter how many kgs you want"))
+cost1=int(input("product1 cost"))
+cost2=int(input("product2 cost"))
+cost3=int(input("product3 cost"))
+totalAmount= product1*cost1 + product2*cost2 + product3*cost3;
+print("total amount would be ", totalAmount)
+#take input from the user : What would be the qty of product1, product 2, product3
+#We will be printing the total the user needs to pay
+
+#expressions
+#If else statements
+#Loops
+
+
+
